786c69b9175a0b0c95ce235ed0e70250b9747370
Kevin <30917136+kev-inc@users.noreply.github.com>
2020-10-06 23:01:46 +0800

    updated env names

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,8 +2,8 @@ var firebase = require("firebase/app");
 require("firebase/database");
 
 const config = {
-  apiKey: process.env.API_KEY,
-  databaseURL: process.env.DB_URL
+  apiKey: process.env.REACT_APP_API_KEY,
+  databaseURL: process.env.REACT_APP_DB_URL
 };
 // Initialize Firebase
 firebase.initializeApp(config);
